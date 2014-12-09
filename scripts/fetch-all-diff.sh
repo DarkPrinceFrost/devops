d5c0aa1a8152f1310b417d3bc56f5c579021bcaf
Ross Reedstrom <reedstrm@gmail.com>
2014-12-09 10:28:18 -0600

    all-singing rewrite git fetcher

---
+++ scripts/fetch-all-diff.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+for d in cnx-archive cnx-authoring cnx-epub cnx-publishing \
+         cnx-query-grammar openstax-accounts plpydbapi webview
+  do (
+    cd $d
+     branchname=$(git symbolic-ref --short -q HEAD)
+    echo -n $d": "
+     git fetch -q
+  echo -n $(git rev-list ^master origin/master| wc -l)
+  if [ $branchname != "master" ]
+  then
+    echo " ($branchname) "$(git rev-list origin/master ^$branchname| wc -l)
+  else
+    echo
+  fi
+if [ "$1" = '-v' ]
+then
+  git log ^master origin/master 
+  git log master ^$branchname
+fi
+   )
+  done
