8f2ddc36c3fd3bbf7a27c305df69627ee58cece8
Ross J. Reedstrom <reedstrm@cnx.org>
2015-02-06 15:34:47 -0600

    place in correct folder

--- scripts/deploy/fetch-all-diff.sh
+++ scripts/deploy/fetch-all-diff.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 for d in cnx-archive cnx-authoring cnx-epub cnx-publishing \
-         cnx-query-grammar openstax-accounts plpydbapi webview
+         cnx-query-grammar openstax-accounts plpydbapi\
+         rhaptos.cnxmlutils webview
   do (
     cd $d
+     branchname=$(git symbolic-ref --short -q HEAD)
     echo -n $d": "
-     git fetch
-     git diff master..origin/master | wc -l
+     git fetch -q
+  echo -n $(git rev-list ^master origin/master| wc -l)
+  if [ $branchname != "master" ]
+  then
+    echo " ($branchname) "$(git rev-list origin/master ^$branchname| wc -l)
+  else
+    echo
+  fi
+if [ "$1" = '-v' ]
+then
+  git log ^master origin/master 
+  git log master ^$branchname
+fi
    )
   done
