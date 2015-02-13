8323bd35b020e588b429c28679d501d951696fea
Reedstrom <reedstrm@cnx.org>
2015-02-13 14:53:40 -0600

    clean up duplicate file storage based on age of files

---
+++ scripts/del_old_dups.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import os
+dir1 = '/var/www/files'
+dir2 = '/var/www/files2'
+
+
+to_del = []
+
+fileset = set(os.listdir(dir1))
+fileset2= set(os.listdir(dir2))
+
+dups = fileset.intersection(fileset2)
+
+for fname in dups:
+    f1=os.path.join(dir1,fname)
+    f2=os.path.join(dir2,fname)
+    if os.stat(f1).st_mtime > os.stat(f2).st_mtime:
+        to_del.append(f2)
+    else:
+        to_del.append(f1)
+
+for d in to_del:
+    print d
+    os.unlink(d)
+
+
+
