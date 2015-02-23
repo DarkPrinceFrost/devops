19afddb1599497b96978673fd93f28eae27854a1
Reedstrom <reedstrm@cnx.org>
2015-02-23 10:55:25 -0600

    compare more than mtime

--- scripts/del_old_dups.py
+++ scripts/del_old_dups.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import filecmp
 dir1 = '/var/www/files'
 dir2 = '/var/www/files2'
 
@@ -14,9 +15,15 @@ dups = fileset.intersection(fileset2)
 for fname in dups:
     f1=os.path.join(dir1,fname)
     f2=os.path.join(dir2,fname)
-    if os.stat(f1).st_mtime > os.stat(f2).st_mtime:
-        to_del.append(f2)
+    if not filecmp.cmp(f1,f2):
+        if os.stat(f1).st_mtime > os.stat(f2).st_mtime:
+            to_del.append(f2)
+        elif os.stat(f1).st_mtime < os.stat(f2).st_mtime:
+            to_del.append(f1)
+        else:
+            print "same timestamp: %s" % (fname,)
     else:
+        print "identical file: %s" % (fname,)
         to_del.append(f1)
 
 for d in to_del:
