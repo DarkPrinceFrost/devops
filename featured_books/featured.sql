d481a1843119bde4b752b56b3fb2375a95b31dd0
Reedstrom <reedstrm@cnx.org>
2015-04-30 12:59:25 -0500

    tagging scripts

---
+++ featured_books/featured.sql
@@ -0,0 +1,4 @@
+INSERT INTO tags (tagid, tag, scheme)
+VALUES (8, 'OpenStax Featured', 'featured'),
+       (9, 'CNX featured', 'featured');
+
---
+++ featured_books/tag_books.sql
@@ -0,0 +1,22 @@
+INSERT INTO moduletags (module_ident, tagid) select module_ident, 9 from modules where uuid in (
+'303800f3-07f3-44d5-a12c-49e93e8948c5',
+'e7b71f2c-a51e-4c9f-8db2-066a4c3643e4',
+'785408b0-910e-4856-aa0c-749a9bb16712',
+'4082db19-1cb8-417c-9605-e7cdaacafeb3',
+'778e36af-4c21-4ef7-9c02-dae860eb7d14',
+'38fa250e-d94d-4ee9-949d-edae08535adc',
+'7e991679-d461-4928-a810-bc3e7c0f21fa',
+'82e6ba6f-b828-42ef-9db1-8de4b448b869',
+'2ad74b7b-a72f-42a9-a31b-7e75542e54bd',
+'7bd5c2b7-22c5-450f-b218-36d260eec58f',
+'d6db603c-cd31-4ca4-be19-d29734673b90',
+'1cb4ffaa-f24a-4b98-8e03-5dd217011f0c',
+'327a14ee-95f6-4c2d-b9a5-3943b4077d36');
+
+INSERT INTO moduletags (module_ident, tagid) select module_ident, 8 from modules where uuid in (
+'031da8d3-b525-429c-80cf-6c8ed997733a',
+'14fb4ad7-39a1-4eee-ab6e-3ef2482e3e22',
+'185cbf87-c72e-48f5-b51e-f14f21b5eabd',
+'30189442-6998-4686-ac05-ed152b91b9de',
+'afe4332a-c97f-4fc4-be27-4e4d384a32d8',
+'b3c1e1d2-839c-42b0-a314-e119a8aafbdd');
