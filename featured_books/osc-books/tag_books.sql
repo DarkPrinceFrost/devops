c2c1888efdb6447f96063d8383c7924cc368abb4
Reedstrom <reedstrm@cnx.org>
2015-10-26 11:27:35 -0500

    more featured book covers

---
+++ featured_books/osc-books/tag_books.sql
@@ -0,0 +1,14 @@
+INSERT INTO moduletags (module_ident, tagid) select module_ident, 8 from modules where uuid in (
+'02040312-72c8-441e-a685-20e9333f3e1d',
+'13ac107a-f15f-49d2-97e8-60ab2e3b519c',
+'33076054-ec1d-4417-8824-ce354efe42d0',
+'4061c832-098e-4b3c-a1d9-7eb593a2cb31',
+'4abf04bf-93a0-45c3-9cbc-2cefd46e68cc',
+'69619d2b-68f0-44b0-b074-a9b2bf90b2c6',
+'85abf193-2bd2-4908-8563-90b8a7ac8df6',
+'8d04a686-d5e8-4798-a27d-c608e4d0e187',
+'9b08c294-057f-4201-9f48-5d6ad992740d',
+'a7ba2fb8-8925-4987-b182-5f4429d48daa',
+'ca344e2d-6731-43cd-b851-a7b3aa0b37aa',
+'ea2f225e-6063-41ca-bcd8-36482e15ef65',
+'fd53eae1-fa23-47c7-bb1b-972349835c3c');
