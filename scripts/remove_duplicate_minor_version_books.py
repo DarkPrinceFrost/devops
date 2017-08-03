d58cdf1af2174f8a29c40082b98fbd0f50f958fe
Ross Reedstrom <reedstrm@gmail.com>
2017-08-03 18:12:18 -0500

    script to remove duplicate minor book versions

---
+++ scripts/remove_duplicate_minor_version_books.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ###
+# Copyright (c) 2017, Rice University
+# This software is subject to the provisions of the GNU Affero General
+# Public License version 3 (AGPLv3).
+# See LICENCE.txt for details.
+# ###
+"""Command-line script to delete minor-version duplicates of books
+"""
+from __future__ import print_function
+
+import argparse
+
+import psycopg2
+
+DEFAULT_PSYCOPG_CONNECTION_STRING = "dbname=cnxarchive user=cnxarchive " \
+                                    "password=cnxarchive host=localhost " \
+                                    "port=5432"
+
+
+def get_dup_idents(cursor):
+    cursor.execute("SELECT ident_hash(uuid,major_version,minor_version),"
+                   "   max(module_ident), array_agg(module_ident)"
+                   " FROM modules"
+                   "    WHERE portal_type = 'Collection'"
+                   " GROUP BY ident_hash(uuid,major_version,minor_version)"
+                   " HAVING count(*) > 1"
+                   )
+    results = cursor.fetchall()
+    for result in results:
+        ident, keep, to_del = result
+        to_del.remove(keep)
+        yield ident, keep, to_del
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser(description='delete duplicate'
+                                                 ' minor versions')
+    parser.add_argument('-d', '--db-conn-str',
+                        default=DEFAULT_PSYCOPG_CONNECTION_STRING,
+                        help='a psycopg2 db connection string')
+    args = parser.parse_args(argv)
+
+    with psycopg2.connect(args.db_conn_str) as db_connection:
+        with db_connection.cursor() as cursor:
+            for ident, keep, to_del in get_dup_idents(cursor):
+                print('Processing {}:'
+                      ' keeping {},'
+                      ' deleting {}'.format(ident, keep, str(to_del)))
+                for table in ('document_baking_result_associations',
+                              'moduletags',
+                              'module_files',
+                              'modules'):
+                    cursor.execute('DELETE FROM {}'
+                                   ' WHERE module_ident'
+                                   ' in %s'.format(table), to_del)
+
+                print('Triggering baking for {}'.format(keep))
+                cursor.execute('UPDATE modules set stateid=5'
+                               ' where module_ident = %s', keep)
+
+
+if __name__ == '__main__':
+    main()
