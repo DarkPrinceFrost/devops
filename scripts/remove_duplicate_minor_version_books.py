153458840cc5fbfa67eacdc12842088dcd4c743a
karen chan <karen@karen-chan.com>
2017-08-07 06:34:05 -0700

    Merge tree changes when removing duplicate minor versions
    Each of the duplicate minor versions was created because a module was
    updated, so we need to update the final tree to include all these
    changes.

--- scripts/remove_duplicate_minor_version_books.py
+++ scripts/remove_duplicate_minor_version_books.py
@@ -34,6 +34,90 @@ def get_dup_idents(cursor):
         yield ident, keep, to_del
 
 
+def merge_trees(cursor, dry_run, ident, keep, merge):
+    """Merge tree updates into one tree.
+
+    Args:
+        cursor: psycopg2 database cursor
+        dry_run (bool): If set to true, the SQL statements are printed out and
+            the database is not modified.
+        ident (str): The ident hash of the collection.
+        keep (int): The module_ident of the collection to keep.
+        merge (list): List of module_idents of the collections to merge into
+            the `keep` tree.
+    """
+    stmt = """\
+WITH RECURSIVE tree1 (nodeid, parentid, path, documentid)
+AS (
+    SELECT tr.nodeid, tr.parent_id, ARRAY[tr.nodeid], tr.documentid
+        FROM trees tr
+        WHERE tr.documentid = %s AND tr.is_collated = FALSE
+    UNION ALL
+    SELECT t.nodeid, t.parent_id, path || ARRAY[t.nodeid], t.documentid
+        FROM trees t JOIN tree1 ON t.parent_id = tree1.nodeid
+        WHERE t.nodeid != ANY(path)
+), tree2 (nodeid, parentid, path, documentid)
+AS (
+    SELECT tr.nodeid, tr.parent_id, ARRAY[tr.nodeid], tr.documentid
+        FROM trees tr
+        WHERE tr.documentid IN %s AND tr.is_collated = FALSE
+    UNION ALL
+    SELECT t.nodeid, t.parent_id, path || ARRAY[t.nodeid], t.documentid
+        FROM trees t JOIN tree2 ON t.parent_id = tree2.nodeid
+        WHERE t.nodeid != ANY(path)
+)
+SELECT nodeid, documentid, m.uuid,
+       ident_hash(m.uuid, m.major_version, m.minor_version)
+    FROM tree1 JOIN modules m ON tree1.documentid = m.module_ident
+    WHERE tree1.documentid IS NOT NULL
+UNION ALL
+SELECT nodeid, documentid, m.uuid,
+       ident_hash(m.uuid, m.major_version, m.minor_version)
+    FROM tree2 JOIN modules m ON tree2.documentid = m.module_ident
+    WHERE NOT EXISTS (
+        SELECT 1
+            FROM tree1
+            WHERE tree1.documentid = tree2.documentid
+    ) AND tree2.documentid IS NOT NULL
+"""
+    cursor.execute(stmt, (keep, tuple(merge)))
+    original_tree = False
+    # for original tree, uuid -> (nodeid, documentid)
+    uuid_to_nodeid = {}
+    # for all trees, uuid -> [documentid, ...]
+    uuid_to_documentids = {}
+    # for all trees, documentid -> ident_hash
+    documentid_to_ident_hash = {}
+
+    for nodeid, documentid, uuid_, ident_hash in cursor.fetchall():
+        if ident_hash == ident:
+            # the original tree is the first one that gets returned
+            original_tree = not uuid_to_nodeid
+            continue
+        if original_tree:
+            uuid_to_nodeid[uuid_] = (nodeid, documentid)
+        uuid_to_documentids.setdefault(uuid_, [])
+        uuid_to_documentids[uuid_].append(documentid)
+        documentid_to_ident_hash[documentid] = ident_hash
+
+    for uuid_, (nodeid, documentid) in uuid_to_nodeid.iteritems():
+        documentids = sorted(uuid_to_documentids[uuid_], reverse=True)
+        new_documentid = documentids[0]
+        if documentid != new_documentid:
+            print('Update tree nodeid {} from {} (documentid={}) '
+                  'to {} (documentid={})'
+                  .format(nodeid, documentid_to_ident_hash[documentid],
+                          documentid,
+                          documentid_to_ident_hash[new_documentid],
+                          new_documentid))
+            stmt = ('UPDATE trees SET documentid = %s '
+                    'WHERE nodeid = %s', (new_documentid, nodeid))
+            if dry_run:
+                print(stmt[0] % stmt[1])
+            else:
+                cursor.execute(*stmt)
+
+
 def main(argv=None):
     parser = argparse.ArgumentParser(description='delete duplicate'
                                                  ' minor versions')
@@ -50,6 +134,7 @@ def main(argv=None):
                 print('\nProcessing {}:'
                       ' keeping {},'
                       ' deleting {}'.format(ident, keep, str(to_del)))
+                merge_trees(cursor, args.dry_run, ident, keep, to_del)
                 for table in ('document_baking_result_associations',
                               'moduletags',
                               'module_files',
